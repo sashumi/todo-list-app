fb6606c2d13eab7a580a3fb4ee8217c4ac354591
Harry Volker <htr.volker@gmail.com>
2020-12-21 17:42:26 +0000

    pytest now collects test reports

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+# Install apt dependencies
 sudo apt-get update
 sudo apt-get install -y python3-pip python3-venv
 
+# Create/activate Python virtual environment
 python3 -m venv venv
 source venv/bin/activate
+
+# Install pip requirements
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov
 
-pytest --cov=application
+# Run pytest
+pytest --cov=application --cov-report html
