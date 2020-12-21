c69546127f1a31dc74651b27113ea6716d3f5f1a
Harry Volker <htr.volker@gmail.com>
2020-12-21 16:37:36 +0000

    fixed venv activation

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install -y python3-pip python3-venv
 
 python3 -m venv venv
-source venv/bin/bash
+source venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install pytest pytest-cov
 
