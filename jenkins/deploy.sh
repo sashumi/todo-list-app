41aceee109ca7498ebcef69336f30c4434ad5645
Harry Volker <htr.volker@gmail.com>
2020-12-21 16:34:42 +0000

    added jenkins script files

---
+++ jenkins/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sudo apt-get update
+
+# Install application to /opt/
+sudo mkdir /opt/todo-list
+sudo cp -r . /opt/todo-list
+
+# Generate service file
+cat << EOF > todo-list.service
+[Unit]
+Description=Todo List
+
+[Service]
+User=jenkins
+Environment=DATABASE_URI=$DATABASE_URI
+Environment=SECRET_KEY=$SECRET_KEY
+ExecStart=/bin/bash /opt/todo-list/startup.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Move service file to systemd
+sudo cp todo-list.service /etc/systemd/system/todo-list.service
+
+# systemd reload/start/stop
+sudo systemctl stop todo-list
+sudo systemctl daemon-reload
+sudo systemctl start todo-list
