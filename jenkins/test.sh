5236ca6682d9aeb60286a9ca0fab647cffad7196
htr-volker <htr.volker@gmail.com>
2021-01-17 14:08:50 +0000

    adding jenkins pipeline configuration

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -5,10 +5,10 @@ sudo apt-get update
 sudo apt-get install -y python3-pip python3-venv unzip chromium-browser
 
 # Install chromedriver
-rm -rf /home/jenkins/chromedriver
-mkdir /home/jenkins/chromedriver
-wget -P /home/jenkins/chromedriver https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-unzip /home/jenkins/chromedriver/chromedriver_linux64.zip -d /home/jenkins/chromedriver
+rm -rf $CHROMEDRIVER_PATH
+mkdir $CHROMEDRIVER_PATH
+wget -P $CHROMEDRIVER_PATH https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+unzip $CHROMEDRIVER_PATH/chromedriver_linux64.zip -d /home/jenkins/chromedriver
 
 # Create/activate Python virtual environment
 python3 -m venv venv
